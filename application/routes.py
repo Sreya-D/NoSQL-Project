e69a7ead6fe07e9ae25cbb97ded78f6a9c2db42e
Sreya D <89724156+Sreya-D@users.noreply.github.com>
2024-04-19 22:32:32 +0530

    Add files via upload

---
+++ application/routes.py
@@ -0,0 +1,14 @@
+from application import app
+from flask import render_template
+from .forms import TodoForm
+
+@app.route("/")
+
+def index():
+    return render_template("view_todos.html", title = "layout page")
+
+@app.route("/add_todo")
+
+def add_todo():
+    form = TodoForm()
+    return render_template("add_todo.html", form = form)
